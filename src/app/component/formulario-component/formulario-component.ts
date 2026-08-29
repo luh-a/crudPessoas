cc403ac0cd2090fc6b92cccc2cc9de9922f16bb0
Anna Luz <anna.mecenas@aluno.senai.br>
2026-08-29 00:01:21 -0300

    criação da estrutura base de pastas

---
+++ src/app/component/formulario-component/formulario-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-component',
+  imports: [],
+  templateUrl: './formulario-component.html',
+  styleUrl: './formulario-component.css',
+})
+export class FormularioComponent {}
---
+++ src/app/component/lista-component/lista-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-component',
+  imports: [],
+  templateUrl: './lista-component.html',
+  styleUrl: './lista-component.css',
+})
+export class ListaComponent {}
