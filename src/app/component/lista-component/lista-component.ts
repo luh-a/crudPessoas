94f98f630fdf11292fe19215e989832fa5b47281
Anna Luz <anna.mecenas@aluno.senai.br>
2026-08-29 02:57:28 -0300

    criação do conteudo html e ts dos componentes lista e home

--- src/app/component/lista-component/lista-component.ts
+++ src/app/component/lista-component/lista-component.ts
@@ -1,9 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Pessoa } from '../../services/pessoaService';
+import { PessoaM } from '../../models/pessoaModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-component.html',
   styleUrl: './lista-component.css',
 })
-export class ListaComponent {}
+export class ListaComponent {
+  nomeBusca = '';
+  todasPessoas: PessoaM[] = [];
+  pessoas: PessoaM[] = [];
+
+  constructor(private router: Router, private pessoaService: Pessoa) {}
+
+  ngOnInit() {
+    this.carregarPessoas();
+  }
+
+  carregarPessoas() {
+    this.pessoaService.listarPessoas().subscribe(dados => {
+      this.todasPessoas = dados;
+      this.pesquisar();
+    });
+  }
+
+  pesquisar(){
+    if (this.nomeBusca.trim() === '') {
+      this.pessoas = this.todasPessoas;
+    } else {
+      const nomeBuscaLower = this.nomeBusca.toLowerCase();
+      this.pessoas = this.todasPessoas.filter(pessoa =>
+        pessoa.nome?.toLowerCase().includes(nomeBuscaLower)
+      );
+    }
+  }
+
+  excluirPessoa(pObjPessoa: PessoaM) {
+    if (confirm(`Deseja realmente excluir a pessoa ${pObjPessoa.nome}?`)) {
+      this.pessoaService.excluirPessoa(Number(pObjPessoa.id));
+      this.carregarPessoas();
+    }
+  }
+
+  buscarPessoaPorId(pObjPessoa: PessoaM) {
+    this.router.navigate(['/cadastro', pObjPessoa.id]);
+  }
+
+}
